963a924e7abc9d73733306d19f8a7b79775723ab
DivyaaSubramaniam <91365734+DivyaaSubramaniam@users.noreply.github.com>
2022-05-14 18:44:08 +0530

    Update util.py

--- util.py
+++ util.py
@@ -2,11 +2,18 @@ import json
 from selenium import webdriver
 
 class Util:
+    """ Base Utility class that helps setting up and tearing down the selenium driver
+    """
     DEFAULT_CONFIG_PATH = "config.json"
     
     _driver: webdriver
 
     def load_driver(self, browser, config = DEFAULT_CONFIG_PATH):
+        """ Loads the desired driver
+        Args:
+            browser [str] : one of 'firefox'/'chrome'
+            config [str] : path to the driver for the desired browser
+        """
         try:
             with open(config) as f:
                 driver_path = json.load(f)['driver'][browser]
